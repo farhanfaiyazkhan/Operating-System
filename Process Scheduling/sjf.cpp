927d336a572b455b3f9159dcab8f89b79b13bee3
Farhan Khan <97162698+farhanfaiyazkhan@users.noreply.github.com>
2024-08-10 04:13:36 +0530

    Shortest Job First

--- Process Scheduling/sjf.cpp
+++ Process Scheduling/sjf.cpp
@@ -5,15 +5,15 @@ class SJF_NON_PREEMPTIVE
 {
 protected:
 
-    int m_n;				// total number of processes
-	int m_clock;			// clock
-	int m_lap;				// last arrived process
+    int m_n;			// total number of processes
+    int m_clock;		// clock
+    int m_lap;			// last arrived process
 	
-	std::vector<int> m_at;	// arrival time
-	std::vector<int> m_bt;	// burst time
-	std::vector<int> m_ct;	// completion time
-	std::vector<int> m_tat; // turnaround time
-	std::vector<int> m_wt;	// waiting time
+    std::vector<int> m_at;	// arrival time
+    std::vector<int> m_bt;	// burst time
+    std::vector<int> m_ct;	// completion time
+    std::vector<int> m_tat; 	// turnaround time
+    std::vector<int> m_wt;	// waiting time
 
     std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> scheduler;
 
